2568be067486ce682325226463d3cd3fcf4247fc
wangqiang <wang.qiang@dayang.com.cn>
2020-05-21 22:45:38 +0800

    增加webpack-dev-server以及热更新，处理css图片引入路径问题

--- src/static/scripts/page1/page1.js
+++ src/static/scripts/page1/page1.js
@@ -1,2 +1,3 @@
 import '../../assets/page1/css/page1.less'
 console.log('来自page1的问候')
+console.log('再试一次')
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,10 @@ module.exports = {
   mode: 'development',
   entry: {
     page1: './src/static/scripts/page1/page1.js',
-    page2: './src/static/scripts/page2/page2.js',
-
+    page2: './src/static/scripts/page2/page2.js'
   },
   output: {
-    filename: 'js/[name].[chunkhash:6].js',
+    filename: 'js/[name].[hash:6].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -22,6 +21,9 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../', // 使用css相对路径，避免图片引入路径出错
+            },
           },
           'css-loader',
           'less-loader',
@@ -49,17 +51,24 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'html/page1.html',
+      filename: 'page1.html',
       template: 'src/views/page1.html',
       chunks: ['page1']
     }),
     new HtmlWebpackPlugin({
-      filename: 'html/page2.html',
+      filename: 'page2.html',
       template: 'src/views/page2.html',
       chunks: ['page2']
     }),
     new MiniCssExtractPlugin({
       filename: "css/[name].[chunkhash:6].css",
     })
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true,
+    overlay: true,
+    hot: true
+  },
 }
